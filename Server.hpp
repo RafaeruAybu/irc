f4e760a2557d8865af7758af9e199283d1697dcc
RafaeruAybu <rafaelajubaltov@gmail.com>
2022-02-04 02:48:29 +0300

    added todo

--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <map>
 #include "Client.hpp"
-// #include <basic_string.h>
 //c headers
 #include <stdio.h>
 #include <unistd.h>
@@ -19,9 +18,9 @@
 #include <arpa/inet.h>
 
 //defines
-#define MAX_USERS 1024
-#define POLL_TIMEOUT 3000 //poll timeout time
-#define BUFF_SIZE 512
+#define MAX_USERS 1024      //user nums in 
+#define POLL_TIMEOUT 3000   //poll timeout time
+#define BUFF_SIZE 512       //fd read buff size
 
 class Serv
 {
@@ -36,14 +35,26 @@ private:
     Serv& operator= (const Serv& other);
 
     //foos
-    int get_listen_sock(int port);
+    int get_listen_sock(int port);      
     void process(int fd, char *buf);
+    //todo implement, maybee more!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!yes
+    void add_user();
+    void delete_user();
+    void add_channel();
+    void delete_channel();
+    void add_user_to_channel();
+    void delete_user_from_channel();
+    void add_user_to_operators();
+    void delete_user_from_operators();
+    void kill();    //it is kiLL
+    void kick();    //it is kiCK
+    void ban();     //тупа бан
     //params
-    int listen_socket;  //listening socket
-    int password;       //servers password (PASS 12345678)
-    int num;              //user num including listener
-    struct pollfd fd_list[MAX_USERS]; //fd list to poll
-    std::vector<Client> users; //userlist
+    int listen_socket;                  //listening socket
+    int password;                       //servers password (PASS 12345678)
+    int num;                            //user num including listener
+    struct pollfd fd_list[MAX_USERS];   //fd list to poll
+    std::vector<Client> users;          //userlist
 
 };
 
