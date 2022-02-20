7607ad8e306743685e0989d79ede5b4e21f9fdc3
dduck <dduck@student.21-school.ru>
2022-02-20 16:53:46 +0300

    add TODO-LIST

---
+++ TODO_LIST.cpp
@@ -0,0 +1,26 @@
+
+
+//TODO: Написать метод для регистрации юзера
+
+/*** Комманды
+ ** Регистрация
+ *      PASS
+ *      NICK
+ *      USER
+ *      OPER
+ *      QUIT
+ *
+ ** Сообщения
+ *      PRIVMSG <receiver>{,<receiver>} <text to be sent>
+ *      NOTICE <nickname> <text> (не нужен ответ)
+ *
+ ** Каналы
+ *      JOIN
+ *      INVITE
+ *      KICK
+ *      MODE - нужно ли делать все флаги и вообще саму комманду?
+ *
+ ** Прочее
+ *      PING
+ */
+
