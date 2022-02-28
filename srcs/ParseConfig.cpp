e0eb005881c97c2380c4b53f277cd3fe89d473e9
RafaeruAybu <rafaelajubaltov@gmail.com>
2022-02-28 21:31:07 +0300

    update branch

---
+++ srcs/ParseConfig.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "../includes/Server.hpp"
+
+Config ParseConfig(char *argv[]) //move from Serv class
+{
+	Config config;
+	config.port = std::string(argv[1]); //convert cstyle to std::string
+	config.password = std::string(argv[2]); //same
+	std::ifstream file("config.txt");
+	std::string str;
+	std::getline(file, config.server_name);
+	std::getline(file, config.operator_password);
+	if (std::getline(file, config.operator_password))
+		throw std::invalid_argument( "Config must only contain two lines: Server name and Operator Password!");
+	file.close();
+	return config;
+}
